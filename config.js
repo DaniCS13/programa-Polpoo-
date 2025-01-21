117f5b2b43063610faefc9e1db8529663b248df1
DaniCS13 <danielcamarasanchez@gmail.com>
2025-01-21 11:21:54 +0100

    dd

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   host: 'v2.core.polpoo.com',  // Cambia esto por el host de tu servidor SFTP
   port: 2222,                    // El puerto por defecto de SFTP es 22
   username: 'polpoo_company_8',   // Cambia esto por tu nombre de usuario
-  password: ''    // Cambia esto por tu contraseña
+  password: 'cSrQtGmB'    // Cambia esto por tu contraseña
 };
